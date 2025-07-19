4be055665f97166f9ec92f20e3854c96fdd1683e
Akash Suklabaidya <akashsuklabaidya593@gmail.com>
2025-07-19 14:48:52 +0530

    Changes made

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,8 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "https://quickeats-g62u.onrender.com/"
+    const url = "https://quickeats-g62u.onrender.com"
+    // const url = "http://localhost:4000"
     const [token, setToken] = useState("")
     const [food_list, setFoodList] = useState([])
 
